ec9b78fc2fa50424ec17543f7d978437599accbb
Mansi Sharma <mansi200799@gmail.com>
2026-04-30 10:02:39 +0530

    .

--- project attendance/project attendance. mangment/app.py
+++ project attendance/project attendance. mangment/app.py
@@ -533,5 +533,6 @@ def upload_profile():
 
     flash("Profile photo updated")
     return redirect(url_for("student"))
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
